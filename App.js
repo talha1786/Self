88a364016078aaa9d7fba908fd062861366bd7a5
talha1786 <chtalhakhan7271@gmail.com>
2021-10-20 01:31:02 -0700

    view tag space

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {View} from 'react-native';
@@ -6,19 +7,64 @@ const App = () => {
   return (
     <View
       style={{
-        backgroundColor: 'red',
+        backgroundColor: '#faf',
         flex: 1,
-        // alignItems: 'center',
-        alignItems: 'flex-end',
-        justifyContent: 'flex-end',
       }}>
       <View
         style={{
-          backgroundColor: '#fff',
-          height: 60,
-          width: 60,
-        }}
-      />
+          height: '60%',
+          width: '100%',
+          backgroundColor: '#afa',
+          //justifyContent: 'space-between',
+          justifyContent: 'space-evenly',
+          flexDirection: 'row',
+        }}>
+        <View
+          style={{
+            height: '100%',
+            width: '40%',
+            backgroundColor: '#dfe',
+            justifyContent: 'space-evenly',
+            flexDirection: 'row',
+          }}>
+          <View
+            style={{
+              backgroundColor: '#ab5',
+              height: 60,
+              width: 60,
+            }}></View>
+
+          <View
+            style={{
+              backgroundColor: '#fff',
+              height: 60,
+              width: 60,
+            }}></View>
+        </View>
+
+        <View
+          style={{
+            height: '100%',
+            width: '40%',
+            backgroundColor: '#a4d',
+            justifyContent: 'space-around',
+            flexDirection: 'row',
+          }}>
+          <View
+            style={{
+              backgroundColor: '#000',
+              height: 60,
+              width: 60,
+            }}></View>
+
+          <View
+            style={{
+              backgroundColor: '#fff',
+              height: 60,
+              width: 60,
+            }}></View>
+        </View>
+      </View>
     </View>
   );
 };
