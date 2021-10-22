9a216aea7af9c6104ac2ef4098ef0aff9dcaf5aa
talha1786 <chtalhakhan7271@gmail.com>
2021-10-21 20:26:23 -0700

    text tag,paddind,margin

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 
 const App = () => {
   return (
@@ -63,6 +63,57 @@ const App = () => {
               height: 60,
               width: 60,
             }}></View>
+        </View>
+      </View>
+      <View
+        style={{
+          backgroundColor: '#fa3f',
+          height: '40%',
+          width: '100%',
+        }}>
+        <View
+          style={{
+            backgroundColor: '#fff',
+            height: '60%',
+            width: '50%',
+            paddingLeft: 10,
+          }}>
+          <View
+            style={{
+              backgroundColor: '#000',
+              height: 70,
+              width: 70,
+              marginTop: 10,
+            }}></View>
+          <View
+            style={{
+              backgroundColor: '#af3d',
+              height: 70,
+              width: 70,
+              marginTop: 10,
+            }}></View>
+        </View>
+
+        <View
+          style={{
+            height: '50%',
+            width: '100%',
+            backgroundColor: '#f54',
+            // alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 26,
+              fontWeight: 'bold',
+              fontStyle: 'italic',
+              textAlign: 'center',
+              color: '#fff',
+              // fontFamily: 'lucida grande',
+            }}
+            numberOfLines={3}>
+            Hello World {'\n'}My Name is Talha Khan
+          </Text>
         </View>
       </View>
     </View>
