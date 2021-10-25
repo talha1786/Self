73d9235cb3c25d97d184745ceebbfb46e246ed5b
talha1786 <chtalhakhan7271@gmail.com>
2021-10-25 01:49:10 -0700

    border  in text input

--- src/screens/basicsTwo/BasicsTwo.js
+++ src/screens/basicsTwo/BasicsTwo.js
@@ -1,29 +1,51 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View, Text, TextInput} from 'react native';
+import {View, Text, TextInput} from 'react-native';
 
 export class BasicsTwo extends React.Component {
   render() {
     return (
       <View
         style={{
-          backgroundColor: '#faf',
+          backgroundColor: '#fff',
           flex: 1,
         }}>
         <View
           style={{
-            height: 100,
-            backgroundColor: '#f54',
+            height: 200,
+            backgroundColor: '#fa2',
             alignItems: 'center',
             justifyContent: 'center',
           }}>
           <TextInput
             style={{
-              backgroundColor: '#faf',
-              height: 50,
+              backgroundColor: '#fff',
               width: '70%',
+              borderColor: 'red',
+              // borderLeftColor: 'red',
+              // borderRightColor: 'red',
+              // borderTopColor: 'red',
+              // borderBottomColor: 'red',
+              borderWidth: 2,
+              // borderLeftWidth: 2,
+              // borderRightWidth: 2,
+              // borderTopWidth: 2,
+              // borderBottomWidth: 2,
+              // borderRadius: 17,
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 10,
+              borderBottomLeftRadius: 10,
+              borderBottomRightRadius: 20,
+              paddingLeft: 20,
+              maxHeight: 80,
+
+              color: 'green',
             }}
             placeholder={'Enter Name'}
+            placeholderTextColor={'red'}
+            // underlineColorAndriod={'#000'}
+            multiline={true}
+            // maxLength={4}
           />
         </View>
       </View>
